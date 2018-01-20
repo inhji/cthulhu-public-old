9478cd0f8fb3989b630298cf7bf19892b4e10ff2
Inhji <johnnie@posteo.de>
2018-01-20 18:39:06 +0100

    put date last in post

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -9,8 +9,9 @@ export default function Template({
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+
         <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+        <p>{frontmatter.date}</p>
       </div>
     </div>
   )
